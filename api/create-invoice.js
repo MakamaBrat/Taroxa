2b71fdafef91f05e2169241db9cc255b9ec7aefd
Makam <vascompanyteam@gmail.com>
2026-06-04 17:15:41 +0200

    KPPRPRP

--- api/create-invoice.js
+++ api/create-invoice.js
@@ -1,7 +1,8 @@
 export default async function handler(req, res) {
-  
-  // Разрешаем запросы с любого домена
+
   res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   res.setHeader("Content-Type", "application/json");
 
   if (req.method === "OPTIONS") {
